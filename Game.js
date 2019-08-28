5620307eff3b997419ee7519fb0666558f65c2d0
Hike-Hu <47766029+Hike-Hu@users.noreply.github.com>
2019-08-28 16:49:49 +0800

    修改

--- Game.js
+++ Game.js
@@ -77,27 +77,36 @@ cc.Class({
         this.index = 0;
         let data = {
             "data": {
-                "lvl": 2, "conf": {
-                    "id": 2, "word": ["似", "心", "如", "止", "水", "流", "延", "年", "益", "寿", "比", "东", "西", "南", "北", "山"],
-                    "idiom": ["东西南北", "寿比南山", "延年益寿", "似水流年", "心如止水"],
-                    "posx": [4, 1, 2, 3, 4, 4, 3, 4, 5, 6, 6, 4, 5, 6, 7, 6], 
-                    "posy": [7, 6, 6, 6, 6, 5, 4, 4, 4, 4, 3, 2, 2, 2, 2, 1],
-                    "answer": [4, 7, 8, 12, 13, 15], "barrier": []
+                "lvl": 7,
+                "conf": {
+                    "id": 7,
+                    "word": ["夕", "阳", "西", "下", "不", "春", "投", "明", "明", "白", "白", "机", "不", "雪", "颠", "倒", "黑", "白", "把"],
+                    "idiom": ["夕阳西下", "阳春白雪", "明明白白", "不明不白", "颠倒黑白", "投机倒把"],
+                    "posx": [5, 6, 7, 8, 4, 6, 2, 4, 5, 6, 7, 2, 4, 6, 1, 2, 3, 4, 2],
+                    "posy": [7, 7, 7, 7, 6, 6, 5, 5, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2],
+                    "answer": [1, 7, 9, 13, 15, 17],
+                    "barrier": []
                 }
             },
-            "errcode": 0, "errmsg": ""
+            "errcode": 0,
+            "errmsg": ""
         };
 
         let _words = data.data.conf.word;
         let _idiom = data.data.conf.idiom;
         let _posx = data.data.conf.posx;
         let _posy = data.data.conf.posy;
+
         let _answer = data.data.conf.answer;
 
         var _letters = new Map();
-        for (let i = 0; i < _words.length;i++)
-        {
+        for (let i = 0; i < _words.length; i++) {
             _letters.set(_words[i], _posx[i] * 10 + _posy[i]);
+        }
+
+        var _index_letters = new Map();
+        for (let i = 0; i < _words.length; i++) {
+            _index_letters.set(_posx[i] * 10 + _posy[i], _words[i]);
         }
 
         for (let i = 0; i < _answer.length; i++) {
@@ -135,119 +144,171 @@ cc.Class({
         //点击下面字的监听 放上去 word_key map中的
         cc.game.on("clickWord", function (id, word) {
             var _wordPrefab = this.qipan.getChildByName(this.oldArr[this.index].toString()).getComponent("WordPrefab");
-
-            
+            let x = _posx[this.selectId];
+            let y = _posy[this.selectId];
+            var word_key = x * 10 + y;
             var right_word = "";
             if (_wordPrefab && _wordPrefab.showWord(id, word)) {
-                let x = _posx[this.selectId];
-                let y = _posy[this.selectId];
-                let word_key = x * 10 + y;
-                _wordPrefab.setPos(x,y);
+
+                // let word_key = x * 10 + y;
+                _wordPrefab.setPos(x, y);
                 if (this.map_temp.has(word_key) && this.map_temp.get(word_key) == 0) {
                     this.map_temp.set(word_key, id);
                 }
 
-                if (this.map_temp.get(word_key) != this.map_answer.get(word_key))
-                {
+                if (this.map_temp.get(word_key) != this.map_answer.get(word_key)) {
                     // 填词错误
                     right_word = _words[this.map_answer.get(word_key)];
                 }
                 else
                     right_word = word;
-                console.log("right :" , right_word);
+                console.log("right :", right_word);
             }
             else
                 return;
 
-            // 判读是否有一个词条完成了任务，或者错误答案
-            for (let i = 0;i < _idiom.length; i++)
+            //判断填写的字所在的位置 含有那些成语
             {
-                let citiao = _idiom[i];
-                // 判断对应字再不在里面
-                let bcontain = false;
-                for (let x = 0; x < citiao.length; x++)
-                {
-                    if (citiao[x] == right_word)
-                    {
-                        bcontain = true;
-                        break;
+                let start_ = 0;
+                let count_ = 0;
+                let end_ = 0;
+                for (let i = 1; i <= 9; i++) {
+                    let temp_word = i * 10 + y;
+                    if (_index_letters.has(temp_word)) {
+                        count_ += 1;
+                    }
+                    else {
+                        count_ = 0;
+                    }
+    
+                    if (count_ == 4) {
+                        end_ = i;
+                        start_ = end_ - 3;
                     }
                 }
-
-                if (bcontain)
-                {
-                    //判断是否已经填满
-                    let bNotFinish = false;
-                    let bRight = true; 
-                    
-                    for (let j = 0; j < citiao.length; j++)
-                    {
-                        let ci = citiao[j];
-                        let _index = _letters.get(ci);
-                        if (this.map_temp.has(_index))
-                        {
-                            if (this.map_temp.get(_index) != this.map_answer.get(_index))
-                            {
+                if (end_ - start_ == 3) {
+                    let bFinish = true;
+                    let bRight = true;
+                    //横向的已经找出 判断是否已经填写满，或者是否正确
+                    for (let i = start_; i <= end_; i++) {
+                        let temp_word = i * 10 + y;
+                        if (this.map_temp.has(temp_word)) {
+                            if (this.map_temp.get(temp_word) == 0) {
+                                bFinish = false;
                                 bRight = false;
                             }
-                            
-                            if (this.map_temp.get(_index) == 0)
-                                bNotFinish = true;
-                        }
-                    }
-
-                    if (bRight)
-                        console.log(citiao + " is right");
-                    else
-                    {
-                        if (bNotFinish == false)
-                            console.log(citiao + " is wrong");
-                        else
-                            console.log(citiao + " is not finished");
-                    }
-                        
-                }
-            }
-            // Global.word.push(word);
-            //一个成语填满了才需要判断是否正确   
-            //填的字的位置下标
-            let clickindex = this.oldArr[this.index];
-            //和这个下标一样的x(也就是竖着的成语) y一样(横着的成语)
-            let num_x = this.dataArr.posx[clickindex];
-            let num_y = this.dataArr.posy[clickindex];
-            let arr_posx = [];
-            let arr_posy = [];
-            let map = [];
-            for(let i=0;i<this.dataArr.posx.length;i++){
-                if(this.dataArr.posx[i] == num_x&&i!=clickindex){
-                    arr_posx.push(i);
-                }
-                if(this.dataArr.posy[i] == num_y&&i!=clickindex){
-                    arr_posy.push(i);
-                }
-            }
-            // console.log("除自己同一竖列的字的下标",arr_posx,"除自己同一横列的字的下标",arr_posy);
-            if(this.dataArr.word[this.oldArr[this.index]] == word){
-                for(let i=0;i<this.dataArr.idiom.length;i++){
-                    if(this.dataArr.idiom[i].indexOf(word.toString())!=-1){
-                        // console.log("word:",word,this.dataArr.idiom[i]);
-                        let num=0;
-                        for(let k=0;k<this.dataArr.idiom[i].length;k++){
-                            for(let j=0;j<Global.word.length;j++){
-                                if(Global.word[j] == this.dataArr.idiom[i][k]){
-                                    num++;
-                                }
+                            else if (this.map_temp.get(temp_word) != this.map_answer.get(temp_word)) {
+                                bRight = false;
                             }
                         }
-                        if(num ==4){
-                            console.log("正确",this.dataArr.idiom[i]);
-                        }
+                    }
+    
+    
+                    if (bRight) {
+                        console.log(`横向 %d,%d is right`, start_, end_);
+                    }
+                    else {
+                        if (bFinish == false)
+                            console.log(`横向 %d,%d is not finished`, start_, end_);
+                        else
+                            console.log(`横向 %d,%d is wrong`, start_, end_);
                     }
                 }
             }
-            this.haveziArr[this.index]=1;
-            for(let i=0;i<this.haveziArr.length;i++){
-                if(this.haveziArr[i]==0){
+            {
+                let start_ = 0;
+                let count_ = 0;
+                let end_ = 0;
+                for (let i = 1; i <= 9; i++) {
+                    let temp_word = x*10 + i;
+                    if (_index_letters.has(temp_word)) {
+                        count_ += 1;
+                    }
+                    else {
+                        count_ = 0;
+                    }
+    
+                    if (count_ == 4) {
+                        end_ = i;
+                        start_ = end_ - 3;
+                    }
+                }
+                if (end_ - start_ == 3) {
+                    let bFinish = true;
+                    let bRight = true;
+                    //横向的已经找出 判断是否已经填写满，或者是否正确
+                    for (let i = start_; i <= end_; i++) {
+                        let temp_word = x*10 + i;
+                        if (this.map_temp.has(temp_word)) {
+                            if (this.map_temp.get(temp_word) == 0) {
+                                bFinish = false;
+                                bRight = false;
+                            }
+                            else if (this.map_temp.get(temp_word) != this.map_answer.get(temp_word)) {
+                                bRight = false;
+                            }
+                        }
+                    }
+    
+    
+                    if (bRight) {
+                        console.log(`纵向 %d,%d is right`, start_, end_);
+                    }
+                    else {
+                        if (bFinish == false)
+                            console.log(`纵向 %d,%d is not finished`, start_, end_);
+                        else
+                            console.log(`纵向 %d,%d is wrong`, start_, end_);
+                    }
+                }
+            }
+
+
+            // 判读是否有一个词条完成了任务，或者错误答案
+            // for (let i = 0; i < _idiom.length; i++) {
+            //     let citiao = _idiom[i];
+            //     // 判断对应字再不在里面
+            //     let bcontain = false;
+            //     for (let x = 0; x < citiao.length; x++) {
+
+            //         if (citiao[x] == right_word) {
+            //             bcontain = true;
+            //             break;
+            //         }
+            //     }
+
+            //     if (bcontain) {
+            //         //判断是否已经填满
+            //         let bNotFinish = false;
+            //         let bRight = true;
+
+            //         for (let j = 0; j < citiao.length; j++) {
+            //             let ci = citiao[j];
+            //             let _index = _letters.get(ci);
+            //             if (this.map_temp.has(_index)) {
+            //                 if (this.map_temp.get(_index) != this.map_answer.get(_index)) {
+            //                     bRight = false;
+            //                 }
+
+            //                 if (this.map_temp.get(_index) == 0)
+            //                     bNotFinish = true;
+            //             }
+            //         }
+
+            //         if (bRight)
+            //             console.log(citiao + " is right");
+            //         else {
+            //             if (bNotFinish == false)
+            //                 console.log(citiao + " is wrong");
+            //             else
+            //                 console.log(citiao + " is not finished");
+            //         }
+
+            //     }
+            // }
+            this.haveziArr[this.index] = 1;
+            for (let i = 0; i < this.haveziArr.length; i++) {
+                if (this.haveziArr[i] == 0) {
                     this.index = i;
                     break;
                 }
